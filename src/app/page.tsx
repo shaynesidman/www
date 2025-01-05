bb414575b2a0925f2a1fcba49b2af06e5305c867
shaynesidman <shaynesidman@Shaynes-MBP.fios-router.home>
2025-01-05 16:03:57 -0500

    Created basic landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-import NavBar from "../components/NavBar"
+import "./globals.css"
 
 export default function Home() {
   return (
-    <div></div>
+    <div className="mainpage">
+      <div className="introduction title">Hi, I'm <span className="tufts">Shayne</span>.</div>
+      <div className="title">I study Computer Science at <span className="tufts">Tufts University</span>.</div>
+      <div className="checkout">Check out some of the work that I have done!</div>
+    </div>
   );
 }
