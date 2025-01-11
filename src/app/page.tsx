565087a28b59f4872922d97a6501c2293e87d43f
shaynesidman <shaynesidman@Shaynes-MBP.fios-router.home>
2025-01-10 20:25:36 -0500

    Designed projects section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,14 @@
-"use client";
-
 import "./globals.css";
-import Coursework from "../components/Coursework";
-import { useRef } from "react";
+import Introduction from "@/components/Introduction";
+import Coursework from "@/components/Coursework";
+import Projects from "@/components/Projects";
 
 export default function Home() {
-  const checkOutRef = useRef<HTMLDivElement>(null);
-
-  const checkOut = () => {
-    checkOutRef.current?.scrollIntoView({ behavior: "smooth" })
-  }
-
   return (
     <div className="mainpage">
-      <div className="intro">
-        <div className="introduction title">Hi, I'm <span className="tufts">Shayne</span>.</div>
-        <div className="title">I study Computer Science at <span className="tufts">Tufts University</span>.</div>
-        <div className="checkout" onClick={checkOut}>Check out some of the work that I have done!</div>
-      </div>
-      <div id="coursework" ref={checkOutRef}><Coursework /></div>
-      <div id="projects"></div>
+      <Introduction />
+      <div id="coursework"><Coursework /></div>
+      <div id="projects"><Projects /></div>
       <div id="experience"></div>
       <div id="aboutme"></div>
     </div>
