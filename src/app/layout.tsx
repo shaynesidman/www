23ccb9c62743e75aa5a988a14a5cf84d07b8111b
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2025-05-06 21:55:01 -0400

    small change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,8 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <style>
-        @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap');        </style>
+          @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+        </style>
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <NavBar />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,16 @@ import "./globals.css";
 import Introduction from "@/components/Introduction";
 import Coursework from "@/components/Coursework";
 import Projects from "@/components/Projects";
+import Experience from "@/components/Experiences";
 
 export default function Home() {
+  {* Home page *}
   return (
     <div className="mainpage">
       <Introduction />
       <div id="coursework"><Coursework /></div>
       <div id="projects"><Projects /></div>
-      <div id="experience"></div>
+      <div id="experience"><Experience /></div>
       <div id="aboutme"></div>
     </div>
   );
