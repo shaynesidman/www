b49d415247b5bbc0beb8a45811ed4c18c0817883
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2025-11-07 14:21:37 -0500

    started refactoring website

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    purge: [],
+    theme: {
+        extend: {
+            fontFamily: {
+                primary: ['var(--font-primary)'],
+                secondary: ['var(--font-secondary)'],
+                mono: ['var(--font-mono)'],
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
